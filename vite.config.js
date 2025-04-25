009597b3c7e040d3fa4f97b8dd7d0f02336ef713
Francis Gallant <francis.gallant1@gmail.com>
2025-04-25 15:31:14 -0400

    fix: final vite config for production build

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: 'public/index.html'
-    }
+    outDir: 'dist'
   },
   server: {
     port: 5173
