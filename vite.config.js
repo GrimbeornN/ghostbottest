02b901f69ddca0a30ef3cacd76724ccd8132aa9a
Francis Gallant <francis.gallant1@gmail.com>
2025-04-25 15:22:25 -0400

    fix: set input public/index.html for Vite build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'public/index.html'
+    }
+  },
   server: {
     port: 5173
   }
